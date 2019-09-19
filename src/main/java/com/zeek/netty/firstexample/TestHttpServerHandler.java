1e53fdeb41d28fa98cd1a2e511146a0fba3e3a9e
zeek <zeek_beijing@126.com>
2019-09-19 19:48:16 +0800

    排除chrome浏览器favicon.ico请求

--- src/main/java/com/zeek/netty/firstexample/TestHttpServerHandler.java
+++ src/main/java/com/zeek/netty/firstexample/TestHttpServerHandler.java
@@ -7,6 +7,8 @@ import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.*;
 import io.netty.util.CharsetUtil;
 
+import java.net.URI;
+
 /**
  * @ClassName TestHttpServerHandler
  * @Description
@@ -21,6 +23,17 @@ public class TestHttpServerHandler extends SimpleChannelInboundHandler<HttpObjec
     protected void channelRead0(ChannelHandlerContext ctx, HttpObject msg) throws Exception {
 
         if(msg instanceof HttpMessage) {
+
+            HttpRequest httpRequest = (HttpRequest) msg;
+
+            System.out.println("请求方法名:" + httpRequest.method().name());
+
+            URI uri = new URI(httpRequest.uri());
+            if ("/favicon.ico".equals(uri.getPath())) {
+                System.out.println("请求favicon.ico");
+                return;
+            }
+
             ByteBuf content = Unpooled.copiedBuffer("Hello World", CharsetUtil.UTF_8);
             FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, content);
             response.headers().set(HttpHeaderNames.CONTENT_TYPE, "text/plain");
