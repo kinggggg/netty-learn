df86809c41eb2ca5352754ecf306cb75992daf35
zeek <zeek_beijing@126.com>
2019-09-20 19:59:33 +0800

    secondexample实例部分代码

---
+++ src/main/java/com/zeek/netty/secondexample/MyServer.java
@@ -0,0 +1,40 @@
+package com.zeek.netty.secondexample;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * @ClassName MyServer
+ * @Description
+ * @Author liweibo
+ * @Date 2019/9/20 7:55 PM
+ * @Version v1.0
+ **/
+public class MyServer {
+
+    public static void main(String[] args) throws Exception {
+
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+
+
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(workerGroup, bossGroup).channel(NioServerSocketChannel.class)
+                    .childHandler(null);
+
+            ChannelFuture channelFuture = serverBootstrap.bind(8899).sync();
+            channelFuture.channel().closeFuture().sync();
+
+        }finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+
+
+    }
+}
