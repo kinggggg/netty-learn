5fc556e0029de658a9745ec435477fbada64e158
zeek <zeek_beijing@126.com>
2019-09-25 16:23:58 +0800

    聊天室程序（部分）

--- src/main/java/com/zeek/netty/thirdexample/MyChatServer.java
+++ src/main/java/com/zeek/netty/thirdexample/MyChatServer.java
@@ -1,11 +1,38 @@
 package com.zeek.netty.thirdexample;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
 /**
  * @ClassName MyChatServer
  * @Description
- * @Author liweibo
  * @Date 2019/9/25 2:57 PM
  * @Version v1.0
  **/
 public class MyChatServer {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+
+
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(workerGroup, bossGroup).channel(NioServerSocketChannel.class)
+                    .childHandler(new MyChatServerInitializer());
+
+            ChannelFuture channelFuture = serverBootstrap.bind(8899).sync();
+            channelFuture.channel().closeFuture().sync();
+
+        }finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+
+    }
 }
--- src/main/java/com/zeek/netty/thirdexample/MyChatServerHandler.java
+++ src/main/java/com/zeek/netty/thirdexample/MyChatServerHandler.java
@@ -1,11 +1,71 @@
 package com.zeek.netty.thirdexample;
 
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
+import io.netty.util.concurrent.GlobalEventExecutor;
+
 /**
  * @ClassName MyChatServerHandler
  * @Description
- * @Author liweibo
  * @Date 2019/9/25 3:07 PM
  * @Version v1.0
  **/
-public class MyChatServerHandler {
+public class MyChatServerHandler extends SimpleChannelInboundHandler<String> {
+
+    // 保存所有已经与服务器端已经建立好的链接的客户端
+    private ChannelGroup channelGroup = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
+        Channel channel = ctx.channel();
+
+        channelGroup.forEach(ch -> {
+            if(channel != ch) {
+                ch.writeAndFlush(channel.remoteAddress() + " 发送的消息：" + msg + "\\n");
+            }else {
+                ch.writeAndFlush("【自己】" + msg + "\\n");
+            }
+        });
+    }
+
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+        Channel channel = ctx.channel();
+
+        // 向所有的客户端发送消息
+        channelGroup.writeAndFlush("【服务器】- " + channel.remoteAddress() + " 加入\\n");
+        channelGroup.add(channel);
+    }
+
+    @Override
+    public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+        Channel channel = ctx.channel();
+
+        channelGroup.writeAndFlush("【服务器】- " + channel.remoteAddress() + " 离开\\n");
+        // 当某个客户端断开连接后Netty会自动地将其从channelGroup中移除！！！！很神奇
+        //channelGroup.remove(channel);
+
+    }
+
+    // 连接处于活动状态
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        Channel channel = ctx.channel();
+        System.out.println(channel.remoteAddress() + " 上线");
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        Channel channel = ctx.channel();
+        System.out.println(channel.remoteAddress() + " 下线");
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
 }
