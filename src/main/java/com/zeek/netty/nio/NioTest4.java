2d8b3f96cfd977bcdab4f93e00144ba636f18a43
zeek <liweibooooo@126.com>
2019-11-20 01:10:18 +0800

    通过nio读写文件

---
+++ src/main/java/com/zeek/netty/nio/NioTest4.java
@@ -0,0 +1,36 @@
+package com.zeek.netty.nio;
+
+import io.netty.buffer.ByteBuf;
+
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+public class NioTest4 {
+
+    public static void main(String[] args) throws IOException {
+        ByteBuffer byteBuf = ByteBuffer.allocate(512);
+
+        FileInputStream inputStream = new FileInputStream("input.txt");
+        FileOutputStream outputStream = new FileOutputStream("output.txt");
+
+        FileChannel inputChannel = inputStream.getChannel();
+        FileChannel outputChannel = outputStream.getChannel();
+
+        while (true) {
+            byteBuf.clear();
+            int read = inputChannel.read(byteBuf);
+            if (read == -1) {
+                break;
+            }
+
+            byteBuf.flip();
+            outputChannel.write(byteBuf);
+        }
+
+        outputChannel.close();
+        outputStream.close();
+        inputChannel.close();
+        inputStream.close();
+    }
+}
