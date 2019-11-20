d9de2e1cd64ce69dc289d2428e81bc1c63b6dc28
zeek <zeek_beijing@126.com>
2019-11-20 20:27:51 +0800

    内存映射文件考察

---
+++ src/main/java/com/zeek/netty/nio/NioTest8.java
@@ -0,0 +1,26 @@
+package com.zeek.netty.nio;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+
+/**
+ * NioTest8
+ * 内存映射文件
+ */
+public class NioTest8 {
+
+    public static void main(String[] args) throws IOException {
+        RandomAccessFile randomAccessFile = new RandomAccessFile("NioTest9.txt", "rw");
+        FileChannel fileChannel = randomAccessFile.getChannel();
+
+        MappedByteBuffer mappedByteBuffer = fileChannel.map(FileChannel.MapMode.READ_WRITE, 0, 5);
+
+        mappedByteBuffer.put(0, (byte)'a');
+        mappedByteBuffer.put(3, (byte)'b');
+
+        randomAccessFile.close();
+    }
+}
