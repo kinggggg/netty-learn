23b792e7db6c7e9ba544f1187fd7220f4c95c19f
zeek <zeek_beijing@126.com>
2019-11-28 16:45:52 +0800

    增加iter.remove();的注释

--- src/main/java/com/zeek/netty/nio/NioTest12.java
+++ src/main/java/com/zeek/netty/nio/NioTest12.java
@@ -39,12 +39,21 @@ public class NioTest12 {
             System.out.println("监听端口: " + ports[i]);
         }
 
+        // keys为所有已经注册的通道
+        Set<SelectionKey> keys = selector.keys();
+        System.out.println("++++>已经注册的通道为: " + keys);
+
         while (true) {
+
+            System.out.println("====>已经注册的通道为: " + keys);
+
             int numbers = selector.select();
             System.out.println("numbers: " + numbers);
 
+            // selectionKeys为所有的产生相应的事件后『激活』（通过阅读资料，在底层其实是操作系统完成的）的通道
+            // 每当一个相应的事件后『激活』与该事件相关的SelectionKey会被添加到该集合中
             Set<SelectionKey> selectionKeys = selector.selectedKeys();
-            System.out.println("selectionKeys: " + selectionKeys);
+            System.out.println("0000>selectionKeys: " + selectionKeys);
 
             Iterator<SelectionKey> iter = selectionKeys.iterator();
             while (iter.hasNext()) {
@@ -57,6 +66,13 @@ public class NioTest12 {
 
                     socketChannel.register(selector, SelectionKey.OP_READ);
 
+                    // 当处理完该激活的SelectionKey后，必须要移出掉！否则，该SelectionKey会被重复处理
+                    // Removing the processed SelectionKey
+                    // Having processed the SelectionKey, we're almost ready to return to the main loop.
+                    // But first wemust remove the processed SelectionKey from the set of selected keys.
+                    // If we do not removethe processed key, it will still be present as an activated key in the main set,
+                    // which would lead us to attempt to process it again.
+                    // We call the iterator's remove() method to remove the processed SelectionKey:
                     iter.remove();
 
                     System.out.println("获得客户端链接: " + socketChannel);
@@ -82,9 +98,12 @@ public class NioTest12 {
 
                     System.out.println("读取: " + bytesRead + ", 来自于: " + socketChannel);
 
+                    // 同上的解释
                     iter.remove();
                 }
             }
+
+            System.out.println("1111>selectionKeys: " + selectionKeys);
         }
 
 
---
+++ src/main/java/com/zeek/netty/socket/Test1_Server3_Client.java
@@ -0,0 +1,11 @@
+package com.zeek.netty.socket;
+
+/**
+ * @ClassName Test1_Server3_Client
+ * @Description
+ * @Author liweibo
+ * @Date 2019/11/27 5:43 PM
+ * @Version v1.0
+ **/
+public class Test1_Server3_Client {
+}
