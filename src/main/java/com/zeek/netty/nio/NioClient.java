10922216b2b794b1ac395c86057f03d36e2d63fa
zeek <zeek_beijing@126.com>
2019-11-28 20:39:37 +0800

    聊天程序服务器端和客户端（部分）

---
+++ src/main/java/com/zeek/netty/nio/NioClient.java
@@ -0,0 +1,86 @@
+package com.zeek.netty.nio;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+import java.time.LocalDateTime;
+import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @ClassName NioClient
+ * @Description
+ * @Author liweibo
+ * @Date 2019/11/28 8:13 PM
+ * @Version v1.0
+ **/
+public class NioClient {
+
+    public static void main(String[] args) throws Exception{
+
+        try {
+
+
+            SocketChannel socketChannel = SocketChannel.open();
+            socketChannel.configureBlocking(false);
+
+            Selector selector = Selector.open();
+            socketChannel.register(selector, SelectionKey.OP_CONNECT);
+
+            socketChannel.connect(new InetSocketAddress("127.0.0.1", 8899));
+
+            while (true) {
+                selector.select();
+                Set<SelectionKey> keySet = selector.selectedKeys();
+                for (SelectionKey selectionKey : keySet) {
+                    if (selectionKey.isConnectable()) {
+                        SocketChannel client = (SocketChannel) selectionKey.channel();
+                        // 是否正在进行连接(看源码)
+                        if (client.isConnectionPending()) {
+                            // 完成连接
+                            client.finishConnect();
+
+                            ByteBuffer writeBuffer = ByteBuffer.allocate(1024);
+                            writeBuffer.put((LocalDateTime.now() + " 连接成功").getBytes());
+                            writeBuffer.flip();
+                            client.write(writeBuffer);
+
+                            ExecutorService executorService = Executors.newSingleThreadExecutor(Executors.defaultThreadFactory());
+                            executorService.submit(() -> {
+                                while (true) {
+                                    try {
+                                        writeBuffer.clear();
+                                        InputStream in;
+                                        InputStreamReader input = new InputStreamReader(System.in);
+                                        BufferedReader br = new BufferedReader(input);
+
+                                        String sendMessage = br.readLine();
+                                        writeBuffer.put(sendMessage.getBytes());
+                                        writeBuffer.clear();
+                                        client.write(writeBuffer);
+                                    } catch (Exception e) {
+
+                                    }
+                                }
+                            });
+                        }
+                    }
+                }
+
+                keySet.clear();
+            }
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ src/main/java/com/zeek/netty/nio/NioServer.java
@@ -0,0 +1,110 @@
+package com.zeek.netty.nio;
+
+import java.io.InputStream;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.channels.*;
+import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
+
+/**
+ * @ClassName NioServer
+ * @Description
+ * @Author liweibo
+ * @Date 2019/11/28 5:28 PM
+ * @Version v1.0
+ **/
+public class NioServer {
+
+    private static Map<String, SocketChannel> clientMap = new HashMap<>();
+
+    public static void main(String[] args) throws Exception{
+
+
+        ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+        serverSocketChannel.configureBlocking(false);
+        ServerSocket serverSocket = serverSocketChannel.socket();
+        serverSocket.bind(new InetSocketAddress(8899));
+
+        Selector selector = Selector.open();
+        SelectionKey registerSelectionKey = serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+
+        // keys为所有已经注册的通道
+        Set<SelectionKey> keys = selector.keys();
+        System.out.println("++++>已经注册的通道为: " + keys);
+
+        while (true) {
+            try {
+                System.out.println("====>已经注册的通道为: " + keys);
+
+                int numbers = selector.select();
+                System.out.println("numbers: " + numbers);
+
+                Set<SelectionKey> selectionKeys = selector.selectedKeys();
+                System.out.println("0000>selectionKeys: " + selectionKeys);
+
+                selectionKeys.forEach(selectionKey -> {
+                    SocketChannel client;
+                    try {
+                        if (selectionKey.isAcceptable()) {
+                            ServerSocketChannel server = (ServerSocketChannel)selectionKey.channel();
+                            client = server.accept();
+                            client.configureBlocking(false);
+                            client.register(selector, SelectionKey.OP_READ);
+
+                            String key = " ["+ UUID.randomUUID().toString() +"] " ;
+                            clientMap.put(key, client);
+
+                            System.out.println("获得客户端链接: " + client);
+
+                        } else if (selectionKey.isReadable()) {
+
+                            client = (SocketChannel)selectionKey.channel();
+
+                            ByteBuffer readBuffer = ByteBuffer.allocate(1024);
+                            int count = client.read(readBuffer);
+                            if (count > 0) {
+                                readBuffer.flip();
+                                Charset charset = Charset.forName("utf-8");
+                                String receiveMessage = String.valueOf(charset.decode(readBuffer).array());
+                                System.out.println(client + ":" + receiveMessage);
+
+                                String senderKey = null;
+                                for (Map.Entry<String, SocketChannel> entry : clientMap.entrySet()) {
+                                    if (client == entry.getValue()) {
+                                        senderKey = entry.getKey();
+                                        break;
+                                    }
+                                }
+
+                                for (Map.Entry<String, SocketChannel> entry : clientMap.entrySet()) {
+                                    SocketChannel value = entry.getValue();
+                                    ByteBuffer writeBuffer = ByteBuffer.allocate(1024);
+                                    writeBuffer.put((senderKey + ":" + receiveMessage).getBytes());
+                                    writeBuffer.flip();
+
+                                    value.write(writeBuffer);
+                                }
+                            }
+
+                            System.out.println("读取消息, 来自于: " + client);
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                });
+                // 我感觉：这里调用clear方法不安全，应该只删除当前的selectionKey
+                selectionKeys.clear();
+
+                System.out.println("1111>selectionKeys: " + selectionKeys);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
