2b2110b4e75f968c755362513439943398b2f481
zeek <liweibooooo@126.com>
2019-11-29 00:27:00 +0800

    字符集考察

---
+++ src/main/java/com/zeek/netty/nio/NioTest13.java
@@ -0,0 +1,56 @@
+package com.zeek.netty.nio;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.CharacterCodingException;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+import java.nio.charset.CharsetEncoder;
+
+public class NioTest13 {
+
+    public static void main(String[] args) throws IOException {
+        String inputFile = "NioTest13_In.txt";
+        String outputFile = "NioTest13_Out.txt";
+
+        RandomAccessFile inputRandomAccessFile = new RandomAccessFile(inputFile, "r");
+        RandomAccessFile outputRandomAccessFile = new RandomAccessFile(outputFile, "rw");
+
+        long inputLength = new File(inputFile).length();
+
+        FileChannel inputFileChannle = inputRandomAccessFile.getChannel();
+        FileChannel outputFileChannel = outputRandomAccessFile.getChannel();
+
+        MappedByteBuffer inputData = inputFileChannle.map(FileChannel.MapMode.READ_ONLY, 0, inputLength);
+
+        System.out.println("===========");
+
+        Charset.availableCharsets().forEach((k, v) -> {
+            System.out.println(k + ", " + v);
+        });
+
+        System.out.println("===========");
+
+
+//        Charset charset = Charset.forName("utf-8");
+        Charset charset = Charset.forName("iso-8859-1");
+        CharsetDecoder decoder = charset.newDecoder();
+        CharsetEncoder encoder = charset.newEncoder();
+
+        CharBuffer charBuffer = decoder.decode(inputData);
+        ByteBuffer outputData = encoder.encode(charBuffer);
+
+        outputFileChannel.write(outputData);
+
+        inputRandomAccessFile.close();
+        outputRandomAccessFile.close();
+
+
+    }
+}
