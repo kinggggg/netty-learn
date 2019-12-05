6f48ad1ef9f27c61d2494e489d658f318755f11a
zeek <liweibooooo@126.com>
2019-12-05 23:53:59 +0800

    分析NioEventLoopGroup类的源码

--- src/main/java/com/zeek/netty/secondexample/MyServer.java
+++ src/main/java/com/zeek/netty/secondexample/MyServer.java
@@ -16,10 +16,20 @@ public class MyServer {
 
     public static void main(String[] args) throws Exception {
 
+        /**
+         * 只是接受连接；死循环；
+         * 主要是做一些初始化的工作，根据传递给构造函数的数值创建相应数量的EventExecutor
+         * 而EventExecutor的作用是：
+         *  The {@link EventExecutor} is a special {@link EventExecutorGroup} which comes
+         *  with some handy methods to see if a {@link Thread} is executed in a event loop.
+         *  Besides this, it also extends the {@link EventExecutorGroup} to allow for a generic
+         *  way to access methods.
+         *  翻译过来就是: EventExecutor是一种特殊的EventExecutorGroup，它提供了一些可以查看一个Thread是否被一个事件循环执行的方便方法。除此之外，它还从EventExecutorGroup中继承了一些访问方法
+         */
+
         EventLoopGroup bossGroup = new NioEventLoopGroup();
+        // 处理真正的请求；死循环
         EventLoopGroup workerGroup = new NioEventLoopGroup();
-
-
 
         try {
             ServerBootstrap serverBootstrap = new ServerBootstrap();
