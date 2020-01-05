484ca46f52fdc91b0ea4fbf2d3bea01d03d08d02
zeek <zeek_beijing@126.com>
2020-01-05 14:40:21 +0800

    MessageToMessageDecoder考察

--- src/main/java/com/zeek/netty/handler/MyLongToStringDecoder.java
+++ src/main/java/com/zeek/netty/handler/MyLongToStringDecoder.java
@@ -8,12 +8,14 @@ import javafx.stage.Stage;
 
 import java.util.List;
 
+// 泛型类型说明了调用该解码器时输入信息的类型
 public class MyLongToStringDecoder extends MessageToMessageDecoder<Long> {
 
     @Override
     protected void decode(ChannelHandlerContext ctx, Long msg, List<Object> out) throws Exception {
         System.out.println("MyLongToStringDecoder invoked!");
 
+        // 这里说明解码器输出内容的类型
         out.add(String.valueOf(msg));
     }
 }
