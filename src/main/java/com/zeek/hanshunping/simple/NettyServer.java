8cb5709c3092992acb9a122d05bd25f3146964ee
liweibo04 <liweibo04@58.com>
2023-01-11 15:40:05 +0800

    为服务器启动添加监听

--- src/main/java/com/zeek/hanshunping/simple/NettyServer.java
+++ src/main/java/com/zeek/hanshunping/simple/NettyServer.java
@@ -2,6 +2,7 @@ package com.zeek.hanshunping.simple;
 
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelOption;
 import io.netty.channel.ChannelPipeline;
@@ -64,6 +65,18 @@ public class NettyServer {
             // 绑定一个端口并且同步, 生成了一个ChannelFuture 对象
             // 启动服务器并绑定端口
             ChannelFuture cf = bootstrap.bind(6668).sync();
+
+            // 给 cf 注册监听器, 监控我们关心的事件
+            cf.addListener(new ChannelFutureListener() {
+                @Override
+                public void operationComplete(ChannelFuture future) throws Exception {
+                    if (cf.isSuccess()) {
+                        System.out.println("监听端口 6668 成功");
+                    } else {
+                        System.out.println("j监听端口 6668 失败");
+                    }
+                }
+            });
 
             // 对关闭通道进行监听
             cf.channel().closeFuture().sync();
